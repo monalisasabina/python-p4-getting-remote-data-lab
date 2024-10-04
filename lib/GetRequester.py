2dce59ea3977470ea15239d0cf52f4859a240d73
Monalisa Onyango <+>
2024-10-05 00:15:43 +0300

    remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+    
+
+results= GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').get_response_body()   
+print(results)
+
+results_json = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').load_json()
+print(results_json)
+print(json.dumps(results_json,indent = 1))
